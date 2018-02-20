448d83b7f2c3151d0a3f961d5632b23b9aa2552a
Yaroslav Nikonorov <yaroslav.n@quali.com>
2018-02-20 16:27:01 +0200

    Addde template files

---
+++ {{cookiecutter.project_slug}}/src/driver.py
@@ -0,0 +1,35 @@
+from cloudshell.shell.core.resource_driver_interface import ResourceDriverInterface
+from cloudshell.shell.core.driver_context import InitCommandContext, ResourceCommandContext, AutoLoadResource, \
+    AutoLoadAttribute, AutoLoadDetails, CancellationContext
+#from data_model import *  # run 'shellfoundry generate' to generate data model classes
+
+
+class {{cookiecutter.driver_name}}(ResourceDriverInterface):
+
+    def __init__(self):
+        """
+        ctor must be without arguments, it is created with reflection at run time
+        """
+        pass
+
+    def initialize(self, context):
+        """
+        Initialize the driver session, this function is called everytime a new instance of the driver is created
+        This is a good place to load and cache the driver configuration, initiate sessions etc.
+        :param InitCommandContext context: the context the command runs on
+        """
+        pass
+
+    def cleanup(self):
+        """
+        Destroy the driver session, this function is called everytime a driver instance is destroyed
+        This is a good place to close any open sessions, finish writing to log files
+        """
+        pass
+
+    def get_inventory(self, context):
+        """ Return device structure with all standard attributes
+        :type context: cloudshell.shell.core.driver_context.AutoLoadCommandContext
+        :rtype: cloudshell.shell.core.driver_context.AutoLoadDetails
+        """
+        pass
